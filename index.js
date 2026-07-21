f4051933a5de166b8fdec931a18ad0a017dd8ef6
Nguyễn Trọng Thức <24110349@student.hcmute.edu.vn>
2026-07-21 14:39:40 +0700

    #2 Thêm quy trình làm việc với express

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
-const express = require('express')
-const app = express()
-const port = 3000;
+const express = require('express');
+const path = require('path'); // Thiết lập đường dẫn 
+
+const app = express();
+const port = 3000; 
+
+// Thiết lập views 
+app.set("views", path.join(__dirname, "views")); //views ở trước không được sửa ./views chính là đường dẫn thiết lập views cho người dùng
+// Chỉ định cho express engine biết templete engine mà bạn đang dùng 
+app.set("view engine", "pug");
 
 app.get('/', (req, res) => {
-    res.send("Trang chủ");
-});
+    res.send("Hello Trang chủ");
+})
 
 app.get('/tours', (req, res) => {
-    res.send("Trang Tour du lịch");
+    res.send("Trang Tour du lịch"); 
 })
 app.listen(port, () => {
-    console.log("Đa nhận đươc tín hiệu từ máy chủ");
+    console.log("Đã nhận tính hiệu từ Web");
 })
+
+//1. Thiết lập dự án yarn init
+//2. cài đặt express
+//3. Cài đặt nodemon
+//4. Thêm pug
