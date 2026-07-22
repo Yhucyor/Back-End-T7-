6108de53ad49ffae07e170834ed897895e9c7cc4
Nguyễn Trọng Thức <24110349@student.hcmute.edu.vn>
2026-07-22 11:09:11 +0700

    #23 TThêm Page Detail Tour và Section 9

--- routes/client/tour.route.js
+++ routes/client/tour.route.js
@@ -3,8 +3,6 @@ const router = require('express').Router(); // Hộ trợ biến Router để th
 const tourController = require('../../controller/client/tourController');
 
 router.get('/', tourController.list)
-router.get('/create', tourController.list)
-router.get('/delete', tourController.list)
-router.get('/edit', tourController.list)
+router.get('/detail', tourController.detail)
 
 module.exports = router;
